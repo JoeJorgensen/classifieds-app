91566e8abd72a43d66310aef0fc7d024768ce5c3
JoeJorgensen <joebjorg@gmail.com>
2022-03-13 22:35:47 -0600

    about page finished and minor tweaks

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -4,7 +4,13 @@ const About = ()=>{
 
     return (
         <div>
-            <h1>About</h1>
+            <h1>About The Developers</h1>
+            <br/>
+            <h3>Dev 1: Joe Jorgensen, pretty easy going guy, just trying to not mess up this app</h3>
+            <br/>
+        
+            <p>That's it that's all of the developers</p>
+
         </div>
     )
 }
